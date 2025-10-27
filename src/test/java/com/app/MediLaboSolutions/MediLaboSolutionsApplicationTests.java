60b80c5695a4e249f46672540cdc4a6d8ac7506c
Houda Ben Abdallah <ahhouda@yahoo.fr>
2025-10-27 13:06:59 +0100

    first commit

---
+++ src/test/java/com/app/MediLaboSolutions/MediLaboSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.MediLaboSolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediLaboSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
